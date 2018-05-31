2e058591861916f6388fbd23002bf583744f3d5e
Octavio Villavicencio Cruz <Octavio.Villavicencio@GFMEGA.COM>
2018-05-31 10:03:51 -0500

    responsive integration

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,26 +1,17 @@
-import { Component, OnInit, ViewChild, AfterViewChecked, DoCheck } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, AfterViewChecked, DoCheck {
+export class NavbarComponent implements OnInit {
 
   @ViewChild('videoPlayer') videoplayer: any;
 
   constructor() { }
 
-  ngOnInit() {}
-
-  ngDoCheck() {
-    console.log(this.videoplayer.nativeElement.paused);
-    if (this.videoplayer.nativeElement.paused) {
-      this.videoplayer.nativeElement.play();
-    }
-  }
-
-  ngAfterViewChecked() {
+  ngOnInit() {
     console.log(this.videoplayer.nativeElement.paused);
     if (this.videoplayer.nativeElement.paused) {
       this.videoplayer.nativeElement.play().ToPromices();
